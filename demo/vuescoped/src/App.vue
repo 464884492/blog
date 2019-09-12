7c327233c5ab7b9e76ffb3768a8f00dc40dd9095
464884492 <464884492@qq.com>
2019-09-12 09:50:17 +0800

    vuescoped demo

---
+++ demo/vuescoped/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <css-scope></css-scope>
+  </div>
+</template>
+
+<style lang="less">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  display:flex;
+  &>div{
+    border:solid 1px #eee;
+    margin:10px;
+    padding:15px;
+  }
+}
+</style>
+
+<script>
+import CssScope from './components/CssScope.vue'
+
+export default {
+  name: 'app',
+  components: {
+    CssScope
+  }
+}
+</script>
+
---
+++ demo/vuescoped/src/components/CssScope.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <van-circle v-model="currentRate" :rate="90" :speed="100" :text="text" />
+  </div>
+</template>
+<style lang="less" scoped>
+.van-circle {
+  /deep/ .van-circle__text {
+    color: blue;
+  }
+}
+</style>
+
+<script>
+import { Circle } from 'vant';
+export default {
+  components: {
+    [Circle.name]: Circle
+  },
+  data() {
+    return {
+      currentRate: 0
+    };
+  },
+  computed: {
+    text() {
+      return this.currentRate.toFixed(0) + '%'
+    }
+  }
+}
+</script>
